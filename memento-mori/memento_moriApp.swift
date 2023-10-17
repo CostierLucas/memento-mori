15119029c6fba264abe9a9542f9115e2be4abc3c
CostierLucas <lucas.costier@outlook.fr>
2023-10-17 14:46:43 +0200

    Initial Commit

---
+++ memento-mori/memento_moriApp.swift
@@ -0,0 +1,17 @@
+//
+//  memento_moriApp.swift
+//  memento-mori
+//
+//  Created by Lucas on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct memento_moriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
